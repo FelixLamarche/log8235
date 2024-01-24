0096da992b665e5f953f0cb48428a9d95efe7b72
dasadou <65833938+dasadou@users.noreply.github.com>
2024-01-24 18:39:17 -0500

    Movement

--- TP1/Source/SoftDesignTraining/SDTAIController.cpp
+++ TP1/Source/SoftDesignTraining/SDTAIController.cpp
@@ -1,13 +1,35 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SDTAIController.h"
 #include "SoftDesignTraining.h"
 
-void ASDTAIController::Tick(float deltaTime)
+ASDTAIController::ASDTAIController()
 {
-
 }
 
+void ASDTAIController::Tick(float deltaTime)
+{
+	APawn* pawn = GetPawn();
 
+	const FVector dir = pawn->GetActorForwardVector();
+	const FVector right = pawn->GetActorRightVector();
 
+	Move(pawn, dir, m_acceleration, deltaTime);
+	Rotate(pawn, right);
 
+	//auto debug_string = FString::Printf(TEXT("Speed: %s"), *m_speed.ToString());
+	//GEngine->AddOnScreenDebugMessage(-1, 0.016f, FColor::Green, debug_string);
+}
+
+void ASDTAIController::Move(APawn* pawn, FVector dir, float acceleration, float deltaTime)
+{
+	auto speed = FMath::Min(m_speed.Size() + deltaTime * acceleration, m_max_speed);
+	m_speed = dir * speed;
+	pawn->AddMovementInput(m_speed);
+}
+
+void ASDTAIController::Rotate(APawn* pawn, FVector dir, float factor)
+{
+	const FRotator current_rot = pawn->GetActorRotation();
+	const FRotator result = FMath::Lerp(current_rot, dir.Rotation(), factor);
+	pawn->SetActorRotation(result);
+}
--- TP1/Source/SoftDesignTraining/SDTAIController.h
+++ TP1/Source/SoftDesignTraining/SDTAIController.h
@@ -8,12 +8,25 @@
 #include "SDTAIController.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS(ClassGroup = AI, config = Game)
 class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
 {
     GENERATED_BODY()
 public:
+    ASDTAIController();
+
     virtual void Tick(float deltaTime) override;
+
+private:
+    void Move(APawn* pawn, FVector dir, float acceleration, float deltaTime);
+    void Rotate(APawn* pawn, FVector dir, float factor = 0.07f);
+
+    UPROPERTY(VisibleAnywhere, Category = AI)
+    FVector m_speed = FVector(0.0, 0.0, 0.0);
+    UPROPERTY(EditAnywhere, Category = AI)
+    double m_max_speed = 150.0;
+    UPROPERTY(EditAnywhere, Category = AI)
+    double m_acceleration = 100.0;
 };
